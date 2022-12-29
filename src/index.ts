68201c4e254153f85ec951b80bd5e05c0c20941d
sktjgudals <sktjgudals@naver.com>
2022-12-29 19:58:52 +0900

    [UPDATE] cors

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
-
+const cors = require("cors");
 const profileImage = require("./router/user/setting/profileimage");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json({ limit: "300mb" }));
 app.use(express.urlencoded({ limit: "300mb", extended: true }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json(false);
-  }
-  next();
-});
 
 app.use("/api/v1/user/setting/profileimage", profileImage);
 app.use("/", (req: Request, res: Response, next: NextFunction) => {
